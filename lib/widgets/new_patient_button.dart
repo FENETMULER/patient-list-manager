b831d785049700089fbf006bd26d389482f4be41
FENET <productivefenetmuler@gmail.com>
2023-04-08 12:36:47 +0300

    Change cursor and add onHover to NewPatientButton

--- lib/widgets/new_patient_button.dart
+++ lib/widgets/new_patient_button.dart
@@ -1,38 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class NewPatientButton extends StatelessWidget {
+class NewPatientButton extends StatefulWidget {
   const NewPatientButton({super.key});
+
+  @override
+  State<NewPatientButton> createState() => _NewPatientButtonState();
+}
+
+class _NewPatientButtonState extends State<NewPatientButton> {
+  Color backgroundColor = const Color(0xff0085FF);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 30, right: 30),
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 15.0),
-          width: 200,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              color: Theme.of(context).primaryColor,
-              boxShadow: const [
-                BoxShadow(
-                  blurRadius: 16.0,
-                  offset: Offset(5, 6),
-                  color: Color.fromARGB(75, 0, 0, 0),
-                )
-              ]),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(FontAwesomeIcons.plus, color: Colors.white),
-              const SizedBox(
-                width: 15.0,
-              ),
-              Text('New Patient',
-                  style: Theme.of(context).textTheme.labelLarge),
-            ],
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onHover: (event) {
+          setState(() {
+            backgroundColor = Theme.of(context).hoverColor;
+          });
+        },
+        onExit: (event) {
+          setState(() {
+            backgroundColor = Theme.of(context).primaryColor;
+          });
+        },
+        child: GestureDetector(
+          onTap: () {
+            print('hello');
+          },
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 15.0),
+            width: 200,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: backgroundColor,
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 16.0,
+                    offset: Offset(5, 6),
+                    color: Color.fromARGB(75, 0, 0, 0),
+                  )
+                ]),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(FontAwesomeIcons.plus, color: Colors.white),
+                const SizedBox(
+                  width: 15.0,
+                ),
+                Text('New Patient',
+                    style: Theme.of(context).textTheme.labelLarge),
+              ],
+            ),
           ),
         ),
       ),
