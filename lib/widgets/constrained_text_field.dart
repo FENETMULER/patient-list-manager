f2060fe92eeccf61e9507ca3ea8621970fdf0129
FENET <productivefenetmuler@gmail.com>
2023-04-14 17:57:43 +0300

    Add validator field to ConstrainedTextField

--- lib/widgets/constrained_text_field.dart
+++ lib/widgets/constrained_text_field.dart
@@ -3,32 +3,35 @@ import 'package:flutter/material.dart';
 class ConstrainedTextField extends StatelessWidget {
   final String label;
   final bool isMultiline;
-  const ConstrainedTextField({required this.label, required this.isMultiline});
+  final String? Function(String?) validator;
+  const ConstrainedTextField(
+      {required this.label,
+      required this.isMultiline,
+      required this.validator});
 
   @override
   Widget build(BuildContext context) {
-    final labelStyle =
-        Theme.of(context).textTheme.labelLarge!.copyWith(color: Colors.grey);
-
-    final floatingLabelStyle = Theme.of(context)
-        .textTheme
-        .labelLarge!
-        .copyWith(color: Theme.of(context).primaryColor);
     return Container(
       margin: const EdgeInsets.only(bottom: 15.0),
       width: 200.0,
-      child: TextField(
+      child: TextFormField(
+        validator: validator,
         maxLines: isMultiline ? 5 : null,
         style: Theme.of(context).textTheme.labelMedium,
-        keyboardType: TextInputType.number,
         decoration: InputDecoration(
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5.0),
             borderSide: const BorderSide(width: 5.0, color: Colors.red),
           ),
           labelText: label,
-          labelStyle: labelStyle,
-          floatingLabelStyle: floatingLabelStyle,
+          labelStyle: Theme.of(context)
+              .textTheme
+              .labelLarge!
+              .copyWith(color: Colors.grey),
+          floatingLabelStyle: Theme.of(context)
+              .textTheme
+              .labelLarge!
+              .copyWith(color: Theme.of(context).primaryColor),
         ),
       ),
     );
