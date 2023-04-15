fe78684d68202919b1424d00d35ad7df970e3491
FENET <productivefenetmuler@gmail.com>
2023-04-15 14:40:36 +0300

    Add patient_services.dart file for database operations

---
+++ lib/services/patient_services.dart
@@ -0,0 +1,17 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+import '../constants/connection.dart';
+import '../models/patient.dart';
+
+var db = Db(connectionString);
+
+void connectDb() async {
+  await db.open();
+}
+
+var coll = db.collection('users');
+
+void dbRegisterPatient(Map<String, dynamic> patientMap) async {
+  var res = await coll.insertOne(patientMap).toString();
+  print(res);
+}
