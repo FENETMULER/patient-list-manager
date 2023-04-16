7a7407d236549118f2e30712558c4978a2f5f04a
FENET <productivefenetmuler@gmail.com>
2023-04-16 16:53:09 +0300

    Use FutureBuilder to get recent patients in home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../widgets/patient_card.dart';
-import '../widgets/update_or_register_patient.dart';
+import '../models/patient.dart';
+import '../services/patient_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String getCurrentDate() {
+  String _getCurrentDate() {
     DateTime today = DateTime.now();
     String formattedDate = DateFormat('MMMM dd, yyyy').format(today);
     return formattedDate;
@@ -32,7 +33,7 @@ class _HomePageState extends State<HomePage> {
 
             // Date
             Text(
-              getCurrentDate(),
+              _getCurrentDate(),
               style: Theme.of(context).textTheme.titleLarge!.copyWith(
                     fontWeight: FontWeight.bold,
                     color: const Color.fromARGB(255, 128, 128, 128),
@@ -48,11 +49,31 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(height: 15.0),
 
             // Recently Registered
-            Column(
-              children: [
-                PatientCard(),
-                PatientCard(),
-              ],
+            FutureBuilder(
+              future:
+                  dbGetRecentPatients(), // getting 5 recently registered patients.
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return Column(
+                      children: snapshot.data!
+                          .sublist(snapshot.data!.length >= 5
+                              ? 4
+                              : snapshot.data!
+                                  .length) // only show a maximum of 5 items if list length is >= 5 else, just show all available elements
+                          .map((patientMap) => PatientCard(
+                                Patient(
+                                  firstName: patientMap['firstName'],
+                                  lastName: patientMap['lastName'],
+                                  age: patientMap['age'],
+                                  sex: patientMap['sex'],
+                                  phoneNumber: patientMap['phoneNumber'],
+                                  registeredOn: patientMap['registeredOn'],
+                                ),
+                              ))
+                          .toList());
+                }
+                return const CircularProgressIndicator();
+              },
             ),
           ],
         ),
