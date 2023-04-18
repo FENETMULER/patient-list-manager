c14148710ed77e019675fe2a37aba31dfc5bda31
FENET <productivefenetmuler@gmail.com>
2023-04-18 20:14:58 +0300

    Add on hover backgroundColor change to patient_card.dart

--- lib/widgets/patient_card.dart
+++ lib/widgets/patient_card.dart
@@ -1,78 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:patient_list_management_system/widgets/modals/patient_details.dart';
 import '../models/patient.dart';
 
-class PatientCard extends StatelessWidget {
+class PatientCard extends StatefulWidget {
   final Patient patient;
+
   const PatientCard({required this.patient});
 
   @override
+  State<PatientCard> createState() => _PatientCardState();
+}
+
+class _PatientCardState extends State<PatientCard> {
+  Color _backgroundColor = Colors.white;
+  double _elevation = 7;
+
+  void displayPatientDetailsModal() {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return PatientDetails(patient: widget.patient);
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 200.0, bottom: 10.0),
-      child: MouseRegion(
-        cursor: SystemMouseCursors.click,
-        child: Card(
-          elevation: 7,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30.0),
-            child: Row(children: [
-              CircleAvatar(
-                child: Text(patient.getInitial,
-                    style: Theme.of(context).textTheme.labelLarge),
-              ),
-              const SizedBox(width: 30.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('${patient.firstName} ${patient.lastName}',
-                      style: Theme.of(context).textTheme.labelMedium),
-                  const SizedBox(height: 3.0),
-                  Row(
-                    children: [
-                      Icon(
-                          patient.sex == 'Male'
-                              ? FontAwesomeIcons.person
-                              : FontAwesomeIcons.personDress,
-                          size: 22.0),
-                      const SizedBox(width: 5.0),
-                      Text(patient.sex,
-                          style: Theme.of(context).textTheme.labelSmall),
-                      const SizedBox(width: 20.0),
-                      Text(
-                        patient.age.toString(),
-                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                            fontSize: 16.0, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(width: 5.0),
-                      Text('Years old',
-                          style: Theme.of(context).textTheme.labelSmall),
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(width: 200.0),
-              Expanded(
-                child: Row(
+    return GestureDetector(
+      onTap: displayPatientDetailsModal,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 200.0, bottom: 10.0),
+        child: MouseRegion(
+          cursor: SystemMouseCursors.click,
+          onEnter: (event) {
+            setState(() {
+              _backgroundColor = const Color(0xFF81CFFF);
+              _elevation = 15;
+            });
+          },
+          onExit: (event) {
+            setState(() {
+              _backgroundColor = Colors.white;
+              _elevation = 7;
+            });
+          },
+          child: Card(
+            color: _backgroundColor,
+            elevation: _elevation,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30.0),
+              child: Row(children: [
+                CircleAvatar(
+                  child: Text(widget.patient.getInitial,
+                      style: Theme.of(context).textTheme.labelLarge),
+                ),
+                const SizedBox(width: 30.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Icon(FontAwesomeIcons.phone, size: 18.0),
-                    const SizedBox(width: 10.0),
-                    Expanded(
-                      child: Text(
-                        patient.phoneNumber,
-                        style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey[600]),
-                      ),
-                    ),
+                    Text(
+                        '${widget.patient.firstName} ${widget.patient.lastName}',
+                        style: Theme.of(context).textTheme.labelMedium),
+                    const SizedBox(height: 3.0),
+                    Row(
+                      children: [
+                        Icon(
+                            widget.patient.sex == 'Male'
+                                ? FontAwesomeIcons.person
+                                : FontAwesomeIcons.personDress,
+                            size: 22.0),
+                        const SizedBox(width: 5.0),
+                        Text(widget.patient.sex,
+                            style: Theme.of(context).textTheme.labelSmall),
+                        const SizedBox(width: 20.0),
+                        Text(
+                          widget.patient.age.toString(),
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelSmall!
+                              .copyWith(
+                                  fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(width: 5.0),
+                        Text('Years old',
+                            style: Theme.of(context).textTheme.labelSmall),
+                      ],
+                    )
                   ],
                 ),
-              )
-            ]),
+                const SizedBox(width: 200.0),
+                Expanded(
+                  child: Row(
+                    children: [
+                      const Icon(FontAwesomeIcons.phone, size: 18.0),
+                      const SizedBox(width: 10.0),
+                      Expanded(
+                        child: Text(
+                          widget.patient.phoneNumber,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyLarge!
+                              .copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[600]),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+            ),
           ),
         ),
       ),
