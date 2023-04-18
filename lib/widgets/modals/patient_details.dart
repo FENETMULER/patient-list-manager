67899d3561ed0cdcb55a812854955d70ba8c6627
FENET <productivefenetmuler@gmail.com>
2023-04-18 18:15:30 +0300

    Add a blurred backdrop filter to patient_details.dart

--- lib/widgets/modals/patient_details.dart
+++ lib/widgets/modals/patient_details.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../main_action_button.dart';
@@ -35,125 +37,128 @@ class PatientDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(35.0, 35.0, 25.0, 15.0),
-        width: 800,
-        height: 470,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Scrollbar(
-              thumbVisibility: true,
-              trackVisibility: true,
-              controller: scrollController,
-              child: SingleChildScrollView(
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+      child: Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(35.0, 35.0, 25.0, 15.0),
+          width: 800,
+          height: 470,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Scrollbar(
+                thumbVisibility: true,
+                trackVisibility: true,
                 controller: scrollController,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            detailBuilder(
+                child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              detailBuilder(
+                                  context: context,
+                                  title: 'First Name',
+                                  value: patient.firstName),
+                              detailBuilder(
                                 context: context,
-                                title: 'First Name',
-                                value: patient.firstName),
-                            detailBuilder(
-                              context: context,
-                              title: 'Last Name',
-                              value: patient.lastName,
-                            ),
-                            detailBuilder(
-                              context: context,
-                              title: 'Age',
-                              value: patient.age.toString(),
-                            ),
-                            detailBuilder(
-                              context: context,
-                              title: 'Sex',
-                              value: patient.sex,
-                            ),
-                            detailBuilder(
-                              context: context,
-                              title: 'Registered On',
-                              value: '{FORMATTED DATE}',
-                            ),
-                          ],
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            detailBuilder(
-                              context: context,
-                              title: 'Phone Number',
-                              value: patient.phoneNumber,
-                            ),
-                            detailBuilder(
+                                title: 'Last Name',
+                                value: patient.lastName,
+                              ),
+                              detailBuilder(
                                 context: context,
-                                title: 'House Number',
-                                value: patient.houseNumber == ''
+                                title: 'Age',
+                                value: patient.age.toString(),
+                              ),
+                              detailBuilder(
+                                context: context,
+                                title: 'Sex',
+                                value: patient.sex,
+                              ),
+                              detailBuilder(
+                                context: context,
+                                title: 'Registered On',
+                                value: '{FORMATTED DATE}',
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              detailBuilder(
+                                context: context,
+                                title: 'Phone Number',
+                                value: patient.phoneNumber,
+                              ),
+                              detailBuilder(
+                                  context: context,
+                                  title: 'House Number',
+                                  value: patient.houseNumber == ''
+                                      ? '---'
+                                      : patient.houseNumber!),
+                              detailBuilder(
+                                context: context,
+                                title: 'Sub-city',
+                                value: patient.subCity == ''
                                     ? '---'
-                                    : patient.houseNumber!),
-                            detailBuilder(
-                              context: context,
-                              title: 'Sub-city',
-                              value: patient.subCity == ''
-                                  ? '---'
-                                  : patient.subCity!,
-                            ),
-                            detailBuilder(
-                              context: context,
-                              title: 'District / Woreda',
-                              value: patient.district == ''
-                                  ? '---'
-                                  : patient.district!,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            detailBuilder(
+                                    : patient.subCity!,
+                              ),
+                              detailBuilder(
                                 context: context,
-                                title: 'Diagnosis',
-                                value: patient.diagnosis == ''
+                                title: 'District / Woreda',
+                                value: patient.district == ''
                                     ? '---'
-                                    : patient.diagnosis!),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+                                    : patient.district!,
+                              ),
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              detailBuilder(
+                                  context: context,
+                                  title: 'Diagnosis',
+                                  value: patient.diagnosis == ''
+                                      ? '---'
+                                      : patient.diagnosis!),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const CancelButton(),
-                const SizedBox(width: 25.0),
-                MainActionButton(
-                    onPressed: () {},
-                    title: 'DELETE',
-                    backgroundColor: Theme.of(context).colorScheme.error),
-                const SizedBox(width: 25.0),
-                MainActionButton(
-                    onPressed: () {},
-                    title: 'UPDATE',
-                    backgroundColor: Theme.of(context).primaryColor),
-              ],
-            )
-          ],
+              const SizedBox(height: 15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const CancelButton(),
+                  const SizedBox(width: 25.0),
+                  MainActionButton(
+                      onPressed: () {},
+                      title: 'DELETE',
+                      backgroundColor: Theme.of(context).colorScheme.error),
+                  const SizedBox(width: 25.0),
+                  MainActionButton(
+                      onPressed: () {},
+                      title: 'UPDATE',
+                      backgroundColor: Theme.of(context).primaryColor),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
